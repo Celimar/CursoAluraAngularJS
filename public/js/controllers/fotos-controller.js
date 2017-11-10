c35f9f5f85f578deb843cdfd65cfd8ab4c7b12c5
Celimar Deoliveira <celimar.deoliveira@ul.com>
2017-11-10 11:50:43 -0200

    capitulo 2

--- public/js/controllers/fotos-controller.js
+++ public/js/controllers/fotos-controller.js
@@ -1,7 +1,21 @@
-angular.module('alurapic').controller('FotosController', function($scope){
+angular.module('alurapic').controller('FotosController', function($scope, $http){
     
-    $scope.foto ={
-        titulo: 'Leão',
-        url: 'http://www.fundosanimais.com/Minis/leao.jpg'
-    }
+    $scope.fotos = [];
+    
+    $http.get('v1/fotos')
+        .success( function(fotos) {
+            $scope.fotos = fotos;
+    })
+    .error( function(error) {
+        console.log(error);    
+    });
+    
+/*
+    var promise = $http.get('v1/fotos');
+    promise.then(function (retorno) {
+        $scope.fotos = retorno.data;
+    }).catch(function error() {
+        console.log(error);
+    });
+// */
 });
