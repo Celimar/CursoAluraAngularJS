dfc986212c0db9c12ac24309e0f39d5201cb82b5
Celimar Oliveira <celimar2@gmail.com>
2017-11-11 21:53:08 -0200

    commit capitulo 7

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,9 +1,18 @@
-angular.module('alurapic').controller('FotoController', function($scope){
+angular.module('alurapic').controller('FotoController', function($scope, $http){
 
     $scope.foto = {};
+    $scope.mensagem = '';
     
     $scope.submeter = function() {
-        console.log($scope.foto);  
+        
+        $http.post('/v1/fotos', $scope.foto)
+        .success(function(){
+            $scope.mensagem = 'Foto adicionada com sucesso';
+        })
+        .error(function(erro) {
+            console.log(erro);   
+            $scope.mensagem = 'Nao foi possivel cadastrar a foto';
+        })
     };
     
 });
